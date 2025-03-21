ca30b600e87c5e330eeb8098655c74b8718b56bd
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-21 18:51:04 +0000

    Create mock_logger.py

---
+++ tests/custom_fixtures/mock_logger.py
@@ -0,0 +1,10 @@
+class MockLogger:
+    class MockLogHandler:
+        def info(self, log_string):
+            pass
+
+        def error(self, log_string):
+            pass
+
+    def __init__(self):
+        self.logger = self.MockLogHandler()
