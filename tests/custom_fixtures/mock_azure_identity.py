d208ff1198bac6de68f4c5bf0728dc6cc3de1b64
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-21 17:03:27 +0000

    Create mock_azure_identity.py

---
+++ tests/custom_fixtures/mock_azure_identity.py
@@ -0,0 +1,27 @@
+from unittest.mock import MagicMock
+from ..custom_fixtures.mock_exceptions import MockClientAuthenticationError
+
+mock_az_token = "mock_token"
+
+mock_mi_credential = MagicMock()
+mock_mi_credential_instance = mock_mi_credential.return_value
+mock_mi_credential_instance.get_token.return_value = mock_az_token
+
+# mock fail
+class MockManagedIdentityCredentialFailCAE:
+    def __iniot__(self):
+        pass
+
+    def get_token(self, url):
+        raise MockClientAuthenticationError(
+          "simulated error MockManagedIdentityCredentialFailCAE"
+        )
+
+class MockManagedIdentityCredentialFailE:
+    def __iniot__(self):
+        pass
+
+    def get_token(self, url):
+        raise Exception(
+          "simulated error MockManagedIdentityCredentialFailE"
+        )
