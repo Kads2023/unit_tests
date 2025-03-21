e6520053fd5f3c7c157965b7e7f7a4ae0f7d4f3c
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-21 19:01:39 +0000

    Create mock_azure_storage.py

---
+++ tests/custom_fixtures/mock_azure_storage.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock
+
+mock_file_content = b"mock_file_content"
+
+mock_azure_storage_dldc = MagicMock()
+mock_azure_storage_dldc_instance = mock_azure_storage_dldc.return_value
+mock_azure_storage_dldc_instance.download_file.return_value.readall.return_value = mock_file_content
+
+
+mock_azure_storage_dldc_fail = MagicMock()
+mock_azure_storage_dldc_instance = (mock_azure_storage_dldc_fail.return_value)
+mock_azure_storage_dldc_instance.download_file.return_value.readall.side_effect = Exception("simulated error general Exception from DataLakeFileClient - Download failed")
+
+
+
+class DataLakeFileClientMock:
+  download_file = MagicMock()
+
+  def --init__(
+    self, account_url, credential, file_system_name, file_path
+  ):
+    self.account_url = account_url
+    self.credential = credential
+    self.file_system_name = file_system_name
+    self.file_path = file_path
+    self.download_file.readall = self._readall()
+
+  def _readall(self):
+    return mock_file_content
