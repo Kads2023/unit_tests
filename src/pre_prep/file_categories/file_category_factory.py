5e9d3e04b5dd2d51f2b655872f461c6727fc9f1d
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-21 17:07:28 +0000

    Update file_category_factory.py

--- src/pre_prep/file_categories/file_category_factory.py
+++ src/pre_prep/file_categories/file_category_factory.py
@@ -5,11 +5,14 @@ from .base_file_categories import BaseFileCategories
 
 class FileCategoriesFactory:
 
+    def __init__(self, lc):
+        self.lc = lc
+  
     def get_file_category_object(self, passed_source_system_type) -> BBaseFileCategories:
         source_system_type = str(passed_source_system_type).lower().strip()
         class_file_name = f"{source_system_type}_file_categories"
         class_name = f"{source_system_type.capitalize()}FileCategories"
         class_module = importlib.import_module(f"pre_pre.file_categories.{class_file_name}")
         class_ref = getattr(class_module, class_name, None)
-        file_category_obj: BaseFileCategories = class_ref()
+        file_category_obj: BaseFileCategories = class_ref(self.lc)
         return file_category_obj
