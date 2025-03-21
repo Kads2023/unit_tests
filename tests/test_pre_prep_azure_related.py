70157153af7b875687d7e898c3e53a6cfda3a6df
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-21 19:18:23 +0000

    Create test_pre_prep_azure_related.py

---
+++ tests/test_pre_prep_azure_related.py
@@ -0,0 +1,13 @@
+# conda create --name <name> python=3.11.0 --no-default-packages
+# create the folders -- D:\<folder_name>\<env_name>
+# conda create --prefix D:\<folder_name>\<env_name> python=3.11.0 --no-default-packages
+# conda install pyspark==3.5.4
+# conda install azure-identity==1.19.0
+# conda install axure-storage-file-datalate==12.17.0
+
+# pytest D:\<folder_name>\test_pre_prep_azure_related.py
+
+
+import pytest
+
+
