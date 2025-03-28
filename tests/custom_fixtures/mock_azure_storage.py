4c7e0fc0e7c6fb7552179952419f3dafc0068abb
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-28 12:29:00 +0000

    Update mock_azure_storage.py

--- tests/custom_fixtures/mock_azure_storage.py
+++ tests/custom_fixtures/mock_azure_storage.py
@@ -1,4 +1,5 @@
 from unittest.mock import MagicMock
+from ..custom_fixtures.mock_exceptions import *
 
 mock_file_content = b"mock_file_content"
 
@@ -9,21 +10,29 @@ mock_azure_storage_dldc_instance.download_file.return_value.readall.return_value
 
 mock_azure_storage_dldc_fail = MagicMock()
 mock_azure_storage_dldc_instance = (mock_azure_storage_dldc_fail.return_value)
-mock_azure_storage_dldc_instance.download_file.return_value.readall.side_effect = Exception("simulated error general Exception from DataLakeFileClient - Download failed")
+mock_azure_storage_dldc_instance.download_file.return_value.readall.side_effect = Exception(
+  "simulated error general Exception from DataLakeFileClient - Download failed"
+)
 
 
+mock_azure_storage_dldc_fail_rsrc = MagicMock()
+mock_azure_storage_dldc_fail_rsrc_instance = (mock_azure_storage_dldc_fail_rsrc_fail.return_value)
+mock_azure_storage_dldc_fail_rsrc_instance.download_file.return_value.readall.side_effect = MockResourceNotFoundError(
+  "simulated error MockResourceNotFoundError from DataLakeFileClient - Download failed"
+)
 
-class DataLakeFileClientMock:
-  download_file = MagicMock()
 
-  def --init__(
-    self, account_url, credential, file_system_name, file_path
-  ):
-    self.account_url = account_url
-    self.credential = credential
-    self.file_system_name = file_system_name
-    self.file_path = file_path
-    self.download_file.readall = self._readall()
+# class DataLakeFileClientMock:
+#   download_file = MagicMock()
 
-  def _readall(self):
-    return mock_file_content
+#   def __init__(
+#     self, account_url, credential, file_system_name, file_path
+#   ):
+#     self.account_url = account_url
+#     self.credential = credential
+#     self.file_system_name = file_system_name
+#     self.file_path = file_path
+#     self.download_file.readall = self._readall()
+
+#   def _readall(self):
+#     return mock_file_content
