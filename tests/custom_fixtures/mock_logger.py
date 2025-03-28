048fff04763a242217a92cff1cabd4bd4d204f3d
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-28 15:11:23 +0000

    Update mock_logger.py

--- tests/custom_fixtures/mock_logger.py
+++ tests/custom_fixtures/mock_logger.py
@@ -1,9 +1,11 @@
 class MockLogger:
     class MockLogHandler:
         def info(self, log_string):
+            # print(log_string)
             pass
 
         def error(self, log_string):
+            # print(log_string)
             pass
 
     def __init__(self):
