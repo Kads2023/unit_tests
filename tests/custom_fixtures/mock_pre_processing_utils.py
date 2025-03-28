40a6d95777ee6464204510a49c4035557efa4068
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-28 15:18:17 +0000

    Create mock_pre_processing_utils.py

---
+++ tests/custom_fixtures/mock_pre_processing_utils.py
@@ -0,0 +1,44 @@
+
+class MockPreProcessingUtilsE:
+  def __init__(self, lc):
+    self.lc = lc
+
+  def get_dict_of_headers_and_strings_to_compare(self):
+    raise Exception(
+      "Mock error from get_dict_of_headers_and_strings_to_compare"
+    )
+    
+  def get_file_details(self):
+    raise Exception(
+      "Mock error from get_file_details"
+    )
+    
+  def replace_special_characters(self):
+    raise Exception(
+      "Mock error from replace_special_characters"
+    )
+    
+  def get_unique_key(self):
+    raise Exception(
+      "Mock error from get_unique_key"
+    )
+    
+  def append_to_report_details_dict(self):
+    raise Exception(
+      "Mock error from append_to_report_details_dict"
+    )
+    
+  def check_and_append_to_report_details_dict(self):
+    raise Exception(
+      "Mock error from check_and_append_to_report_details_dict"
+    )
+    
+  def collate_report_details_dict(self):
+    raise Exception(
+      "Mock error from collate_report_details_dict"
+    )
+    
+  def get_file_details_and_data(self):
+    raise Exception(
+      "Mock error from get_file_details_and_data"
+    )
