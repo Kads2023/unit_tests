e6356d981b1bfe083984b1ab82384a6d61095eca
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-28 13:42:05 +0000

    Update mock_base_file_category.py

--- tests/custom_fixtures/mock_base_file_category.py
+++ tests/custom_fixtures/mock_base_file_category.py
@@ -4,10 +4,11 @@ from pre_prep.file_categories.base_file_categories import BaseFileCategories
 class MockFileCategoriesFactory:
   def __init__(self, lc):
     self.lc = lc
+    self.this_class_name = f"{type(self).__name__}"
 
   de get_file_category_object(self, passed_source_system_type) -> BBaseFileCategories:
     raise Exception(
-      f"[{type(self).__name__}.get_file_category_object()] - "
+      f"[{self.this_class_name}.get_file_category_object()] - "
       f"Mock Exception"
     )
 
@@ -31,17 +32,23 @@ class MockBaseFileCategories(BaseFileCategories):
 class MockBaseFileCategoriesE(BaseFileCategories):
     def __init__(self, lc):
         self.lc = lc
+        self.this_class_name = f"{type(self).__name__}"
+        raise Exception(
+          f"[{self.this_class_name}.__init__()] - "
+          f"keys do not match, "
+          f"check class implementation, "
+          f"list_of_files_keys")
   
     def get_list_of_files_to_process(self, passed_file_type_to_process):
       raise Exception(
-        f"[{type(self).__name__}.get_list_of_files_to_process()] - "
+        f"[{self.this_class_name}.get_list_of_files_to_process()] - "
         f"{passed_file_type_to_process} "
         f"not available as key in files_to_process dictionary"
       )
 
     def get_file_columns_and_schema_dict(self, passed_file_type_to_process):
       raise Exception(
-        f"[{type(self).__name__}.get_file_columns_and_schema_dict()] - "
+        f"[{self.this_class_name}.get_file_columns_and_schema_dict()] - "
         f"{passed_file_type_to_process} "
         f"not available as key in file_columns_and_schema dictionary"
       )
