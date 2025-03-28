40be8c046cf7ccf491b867ec6678f457d4c56e96
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-28 15:10:45 +0000

    Update mock_dbutils.py

--- tests/custom_fixtures/mock_dbutils.py
+++ tests/custom_fixtures/mock_dbutils.py
@@ -1,15 +1,13 @@
-from collections import namedtuple
 from unitest.mock import MagicMock
 
+from .mock_file_info import FileInfoMock
 
-class DbutilsMock;
+class DbutilsMock:
   fs = MagicMock()
 
   def __init__(self, fs_ls_list: list = None)
     if fs_ls_list:
-      self.file_info = namedtuple(
-        typename:"FileInfo", field_names:["path", "name", "size", "modificationTime"]
-      )
+      self.file_info = list[FileInfoMock] = []
       self.fs._ls_list = fs_ls_list
       self.fs.ls = self._dbutils_fs_ls
 
@@ -17,14 +15,32 @@ class DbutilsMock;
     # path parameter is ignored and mock response returned
     file_info = []
     for filename in self.fs._ls_list:
-      file_info.append(
-        self.file_info(
-          "abfss://volume@storage_account_name.dfs.core.windows.net/volume/"
-          + filename,
-          filename,
-          100000,
-          1739381597,
-        )
-      )
+      now_file_info = FileInfoMock(filename)
+      file_info.append(now_file_info)
     return file_info
-      
+
+
+class DbutilsMockFailFNF:
+  fs = MagicMock()
+
+  def __init__(self, fs_ls_list: list = None):
+    self.fs.ls = self._dbutils_fs_ls
+
+  def _dbutils_fs_ls(self, path):
+    # path parameter is ignored and mock response returned
+    raise FileNotFoundError(
+      "simulated error MockFileNotFoundError from DBUtils - File not found"
+    )
+
+
+class DbutilsMockFailF:
+  fs = MagicMock()
+
+  def __init__(self, fs_ls_list: list = None):
+    self.fs.ls = self._dbutils_fs_ls
+
+  def _dbutils_fs_ls(self, path):
+    # path parameter is ignored and mock response returned
+    raise Exception(
+      "simulated error MockDbutilsE"
+    )
