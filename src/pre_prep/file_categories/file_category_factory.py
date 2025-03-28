9e22fc595d62b419c7a7be098ca3762fddd89624
Kads2023 <149354911+Kads2023@users.noreply.github.com>
2025-03-28 12:56:13 +0000

    Update file_category_factory.py

--- src/pre_prep/file_categories/file_category_factory.py
+++ src/pre_prep/file_categories/file_category_factory.py
@@ -1,5 +1,4 @@
 import importlib
-
 from .base_file_categories import BaseFileCategories
 
 
@@ -9,10 +8,26 @@ class FileCategoriesFactory:
         self.lc = lc
   
     def get_file_category_object(self, passed_source_system_type) -> BBaseFileCategories:
-        source_system_type = str(passed_source_system_type).lower().strip()
-        class_file_name = f"{source_system_type}_file_categories"
-        class_name = f"{source_system_type.capitalize()}FileCategories"
-        class_module = importlib.import_module(f"pre_pre.file_categories.{class_file_name}")
-        class_ref = getattr(class_module, class_name, None)
-        file_category_obj: BaseFileCategories = class_ref(self.lc)
-        return file_category_obj
+        this_module = f"[{type(self).__name__}.get_file_category_object()] -"
+        try:
+            source_system_type = str(passed_source_system_type).lower().strip()
+            class_file_name = f"{source_system_type}_file_categories"
+            class_name = f"{source_system_type.capitalize()}FileCategories"
+            class_module = importlib.import_module(f"pre_pre.file_categories.{class_file_name}")
+            class_ref = getattr(class_module, class_name, None)
+            file_category_obj: BaseFileCategories = class_ref(self.lc)
+            return file_category_obj
+        except ModuleNotFoundError as ex:
+            error_msg = (f"{this_module} UNKNOWN: "
+                         f"source_system_type -- {passed_source_system_type}, "
+                         f"Implementation available for "
+                         f"banking / bond / risk")
+            print error_msg
+            raise
+        except Exception as e:
+            error_msg = (f"{this_module} UNKNOWN: "
+                         f"source_system_type -- {passed_source_system_type}, "
+                         f"({e})")
+            print error_msg
+            raise
+            
